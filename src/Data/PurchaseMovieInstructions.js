81d49d00f79c0f76be9ff0850c431701cc8f21d3
Pushpitha17 <pushpithagee@gmail.com>
2021-03-10 10:52:22 +0530

    Instructions Add

--- src/Data/PurchaseMovieInstructions.js
+++ src/Data/PurchaseMovieInstructions.js
@@ -24,9 +24,9 @@ export const purchaseMovie = [
 		tv: tv_0,
 		instruction: {
 			image: menubtn,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			english: 'Press Menu Button to access Home Menu',
+			tamil: 'அணுக மெனு பொத்தானை அழுத்தவும் முகப்பு மெனு',
+			sinhala: 'ප්‍රධාන මෙනුව වෙත ප්‍රවේශ වීමට මෙනු බොත්තම ඔබන්න',
 		},
 	},
 	{
@@ -34,9 +34,12 @@ export const purchaseMovie = [
 		remote: Remote_2,
 		instruction: {
 			image: false,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			english:
+				'Use < or > buttons to navigate through Main Menu items until MOVIES is highlighted ',
+			tamil:
+				'MOVIES சிறப்பம்சமாக இருக்கும் வரை முதன்மை பட்டி உருப்படிகள் வழியாக செல்ல <அல்லது> பொத்தான்களைப் பயன்படுத்தவும்',
+			sinhala:
+				'MOVIES ඉස්මතු වන තෙක් ප්‍රධාන මෙනු අයිතම හරහා සැරිසැරීමට < හෝ > බොත්තම් භාවිතා කරන්න',
 		},
 	},
 	{
@@ -44,56 +47,67 @@ export const purchaseMovie = [
 		remote: Remote_3,
 		instruction: {
 			image: false,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			english: 'Press OK Button to select MOVIES option to see Movie Catalogue',
+			tamil:
+				'மூவி பட்டியலைக் காண MOVIES விருப்பத்தைத் தேர்ந்தெடுக்க சரி பொத்தானை அழுத்தவும்',
+			sinhala:
+				'චිත්‍රපට නාමාවලිය බැලීම සඳහා  MOVIES විකල්පය තේරීමට OK බොත්තම ඔබන්න',
 		},
 	},
 	{
 		tv: tv_3,
 		remote: Remote_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			image: false,
+			english:
+				'Movie Catalogue is displayed with New Arrivals highlighted in the Top Left Menu and Movies Carousel at the middle showing all the newly added movies as movie posters',
+			tamil:
+				'மேல் இடது மெனு மற்றும் மூவி கொணர்வி ஆகியவற்றில் சிறப்பிக்கப்பட்டுள்ள புதிய வருகைகளுடன் திரைப்பட பட்டியல் காட்டப்படும், புதிதாக சேர்க்கப்பட்ட அனைத்து திரைப்படங்களையும் திரைப்பட சுவரொட்டிகளாகக் காட்டுகிறது',
+			sinhala:
+				'ඉහළ වම් මෙනුවෙහි New Arrivals ඉස්මතු කරමින් චිත්‍රපට නාමාවලිය ප්‍රදර්ශනය කෙරෙන අතර මැදින් චිත්‍රපට කැරූසල් (Movies Carousel) අලුතින් එකතු කරන ලද සියලුම චිත්‍රපට චිත්‍රපට පෝස්ටර් ලෙස පෙන්වයි',
 		},
 	},
 	{
 		tv: tv_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			image: false,
+			english: `Use < or > buttons to browse through Movies Carousel  to highlight your desired movie in the middle of Carousel 
+Movie Description Window on the Top Right Corner shows brief description of the highlighted movie`,
+			tamil:
+				'கொணர்விக்கு நடுவில் நீங்கள் விரும்பிய திரைப்படத்தை முன்னிலைப்படுத்த மூவிஸ் கொணர்வி மூலம் உலாவ <அல்லது> பொத்தான்களைப் பயன்படுத்தவும்',
+			sinhala:
+				'කැරොසල් මධ්‍යයේ ඔබේ අපේක්ෂිත චිත්‍රපටය ඉස්මතු කිරීමට චිත්‍රපට කැරොසල් හරහා සැරිසැරීමට < හෝ > බොත්තම් භාවිතා කරන්න',
 		},
 	},
 	{
 		tv: tv_5,
 		remote: Remote_1,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			image: false,
+			english: 'Press OK Button to see Synopsis of the movie',
+			tamil: 'திரைப்படத்தின் சுருக்கத்தைக் காண சரி பொத்தானை அழுத்தவும்',
+			sinhala: 'චිත්‍රපටයේ සාරාංශය බැලීමට OK බොත්තම ඔබන්න',
 		},
 	},
 	{
 		tv: tv_6,
 		remote: Remote_1,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
-			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			image: false,
+			english:
+				'Use ↑ or ↓ buttons to navigate through Top Left Menu for categorizations of movies such as',
+			tamil:
+				'போன்ற திரைப்படங்களின் வகைப்படுத்தல்களுக்கு மேல் இடது மெனு வழியாக செல்ல ↑ அல்லது ↓ பொத்தான்களைப் பயன்படுத்தவும்;',
+			sinhala:
+				'පහත දැක්වෙන ආකාරයේ චිත්‍රපට වර්ගීකරණය සඳහා ඉහළ වම් මෙනුව හරහා සැරිසැරීමට ↑ හෝ ↓ බොත්තම් භාවිතා කරන්න;',
 		},
 	},
 	{
 		tv: tv_7,
 		remote: Remote_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Select the language – Sinhala press OK',
 			tamil: 'பத்திரிகை மெனு',
 			sinhala: 'ඔබන්න මෙනුව',
 		},
@@ -102,8 +116,8 @@ export const purchaseMovie = [
 		tv: tv_8,
 		remote: Remote_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Select the Genre – Children press OK',
 			tamil: 'பத்திரிகை மெனு',
 			sinhala: 'ඔබන්න මෙනුව',
 		},
@@ -112,8 +126,8 @@ export const purchaseMovie = [
 		tv: tv_9,
 		remote: Remote_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Select the Movie – Thaala press OK',
 			tamil: 'பத்திரிகை மெனு',
 			sinhala: 'ඔබන්න මෙනුව',
 		},
@@ -122,8 +136,8 @@ export const purchaseMovie = [
 		tv: tv_10,
 		remote: Remote_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Press OK',
 			tamil: 'பத்திரிகை மெனு',
 			sinhala: 'ඔබන්න මෙනුව',
 		},
@@ -132,8 +146,8 @@ export const purchaseMovie = [
 		tv: tv_11,
 		remote: Remote_5,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Enter 111111  and Press OK',
 			tamil: 'பத்திரிகை மெனு',
 			sinhala: 'ඔබන්න මෙනුව',
 		},
@@ -142,8 +156,8 @@ export const purchaseMovie = [
 		tv: tv_12,
 		remote: Remote_4,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Press OK',
 			tamil: 'பத்திரிகை மெனு',
 			sinhala: 'ඔබන්න මෙනුව',
 		},
@@ -151,10 +165,10 @@ export const purchaseMovie = [
 	{
 		tv: tv_13,
 		instruction: {
-			image: menubtn,
-			english: 'Press menu',
+			image: false,
+			english: 'Press OK',
 			tamil: 'பத்திரிகை மெனு',
-			sinhala: 'ඔබන්න මෙනුව',
+			sinhala: 'ඔබන්න මනුව',
 		},
 	},
 ]
